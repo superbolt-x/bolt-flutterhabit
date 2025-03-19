ce1d4cfb0235c0af2dffda42a78ba33f31c02eb1
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-03-19 13:21:54 -0600

    Update tiktok_ad_performance.sql

--- models/reporting/tiktok_ad_performance.sql
+++ models/reporting/tiktok_ad_performance.sql
@@ -21,11 +21,6 @@ cost as spend,
 impressions,
 clicks,
 CASE WHEN campaign_name ~* 'TTS' THEN conversions ELSE complete_payment_events END as purchases,
-CASE WHEN campaign_id::varchar = '1809207092406273' THEN total_onsite_shopping_value ELSE complete_payment_value END as revenue,
+complete_payment_value as revenue,
 web_add_to_cart_events as atc
 FROM {{ ref('tiktok_performance_by_ad') }}
-LEFT JOIN 
-    (SELECT stat_time_day::date as date, campaign_id::varchar as campaign_id, COALESCE(SUM(total_onsite_shopping_value),0) as total_onsite_shopping_value
-    FROM {{ source('tiktok_raw','campaign_report_daily') }} 
-    WHERE campaign_id = '1809207092406273'
-    GROUP BY 1,2) USING(campaign_id, date)
