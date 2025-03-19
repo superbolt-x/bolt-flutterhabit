4165deb6b30642b8126902c64b1e9af1d3336069
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-03-19 13:21:09 -0600

    Create tiktok_campaign_performance.sql

---
+++ models/reporting/tiktok_campaign_performance.sql
@@ -0,0 +1,23 @@
+{{ config (
+    alias = target.database + '_tiktok_campaign_performance'
+)}}
+
+SELECT 
+campaign_name,
+campaign_id::varchar,
+campaign_status,
+campaign_type_default,
+date,
+date_granularity,
+cost as spend,
+impressions,
+clicks,
+CASE WHEN campaign_name ~* 'TTS' THEN conversions ELSE complete_payment_events END as purchases,
+CASE WHEN campaign_id::varchar = '1809207092406273' THEN total_onsite_shopping_value ELSE complete_payment_value END as revenue,
+web_add_to_cart_events as atc
+FROM {{ ref('tiktok_performance_by_campaign') }}
+LEFT JOIN 
+    (SELECT stat_time_day::date as date, campaign_id::varchar as campaign_id, COALESCE(SUM(total_onsite_shopping_value),0) as total_onsite_shopping_value
+    FROM {{ source('tiktok_raw','campaign_report_daily') }} 
+    WHERE campaign_id = '1809207092406273'
+    GROUP BY 1,2) USING(campaign_id, date)
