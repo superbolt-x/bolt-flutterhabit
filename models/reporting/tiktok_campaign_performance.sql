8f87e714d4fbbb9c7147f4451a8d4d77a3f2b975
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-04-15 18:18:37 +0200

    Update tiktok_campaign_performance.sql

--- models/reporting/tiktok_campaign_performance.sql
+++ models/reporting/tiktok_campaign_performance.sql
@@ -17,7 +17,7 @@ CASE WHEN campaign_name ~* 'TTS' THEN total_onsite_shopping_value ELSE complete_
 web_add_to_cart_events as atc
 FROM {{ ref('tiktok_performance_by_campaign') }}
 LEFT JOIN 
-    (SELECT stat_time_day::date as date, campaign_id::varchar as campaign_id, COALESCE(SUM(total_onsite_shopping_value),0) as total_onsite_shopping_value
+    (SELECT stat_time_day::date as date, campaign_id, COALESCE(SUM(total_onsite_shopping_value),0) as total_onsite_shopping_value
     FROM {{ source('tiktok_raw','campaign_report_daily') }} 
     WHERE campaign_id = '1809207092406273'
     GROUP BY 1,2) USING(campaign_id, date)
