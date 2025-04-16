188ba509cf06d4c41d2a669fb9cc9f7694ae8e23
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-04-16 17:25:37 +0200

    Update tiktok_ad_performance.sql

--- models/reporting/tiktok_ad_performance.sql
+++ models/reporting/tiktok_ad_performance.sql
@@ -20,7 +20,7 @@ date_granularity,
 cost as spend,
 impressions,
 clicks,
-CASE WHEN campaign_name ~* 'TTS' THEN conversions ELSE complete_payment END as purchases,
+CASE WHEN campaign_name ~* 'TTS' THEN conversion ELSE complete_payment END as purchases,
 CASE WHEN campaign_name ~* 'TTS' THEN total_onsite_shopping_value ELSE total_complete_payment_rate END as revenue,
 web_event_add_to_cart as atc
 FROM {{ ref('tiktok_performance_by_ad') }}
