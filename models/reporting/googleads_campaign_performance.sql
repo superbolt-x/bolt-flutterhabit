e7e48cd239c246266ab6af416c606c3596b926ca
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-06-26 15:14:26 -0600

    Update googleads_campaign_performance.sql

--- models/reporting/googleads_campaign_performance.sql
+++ models/reporting/googleads_campaign_performance.sql
@@ -13,8 +13,8 @@ date_granularity,
 spend,
 impressions,
 clicks,
-raw.purchases,
-raw.revenue,
+purchases_adj,
+revenue_adj,
 search_impression_share,
 search_budget_lost_impression_share,
 search_rank_lost_impression_share
@@ -22,37 +22,37 @@ FROM {{ ref('googleads_performance_by_campaign') }}
 LEFT JOIN
     (SELECT DATE_TRUNC('day',date) as date, 'day' as date_granularity, 
     customer_id as account_id, name as campaign_name, campaign_id,
-    COALESCE(SUM(conversions),0) as purchases, COALESCE(SUM(conversions_value),0) as revenue
+    COALESCE(SUM(conversions),0) as purchases_adj, COALESCE(SUM(conversions_value),0) as revenue_adj
     FROM {{ source('googleads_raw','campaign_convtype_performance_report') }}
     WHERE conversion_action_name = 'Google Shopping App Purchase (1)'
     GROUP BY 1,2,3,4,5
     UNION ALL
     SELECT DATE_TRUNC('week',date) as date, 'week' as date_granularity, 
     customer_id as account_id, name as campaign_name, id as campaign_id,
-    COALESCE(SUM(conversions),0) as purchases, COALESCE(SUM(conversions_value),0) as revenue
+    COALESCE(SUM(conversions),0) as purchases_adj, COALESCE(SUM(conversions_value),0) as revenue_adj
     FROM {{ source('googleads_raw','campaign_convtype_performance_report') }}
     WHERE conversion_action_name = 'Google Shopping App Purchase (1)'
     GROUP BY 1,2,3,4,5
     UNION ALL
     SELECT DATE_TRUNC('month',date) as date, 'month' as date_granularity, 
     customer_id as account_id, name as campaign_name, id as campaign_id,
-    COALESCE(SUM(conversions),0) as purchases, COALESCE(SUM(conversions_value),0) as revenue
+    COALESCE(SUM(conversions),0) as purchases_adj, COALESCE(SUM(conversions_value),0) as revenue_adj
     FROM {{ source('googleads_raw','campaign_convtype_performance_report') }}
     WHERE conversion_action_name = 'Google Shopping App Purchase (1)'
     GROUP BY 1,2,3,4,5
     UNION ALL
     SELECT DATE_TRUNC('quarter',date) as date, 'quarter' as date_granularity, 
     customer_id as account_id, name as campaign_name, id as campaign_id,
-    COALESCE(SUM(conversions),0) as purchases, COALESCE(SUM(conversions_value),0) as revenue
+    COALESCE(SUM(conversions),0) as purchases_adj, COALESCE(SUM(conversions_value),0) as revenue_adj
     FROM {{ source('googleads_raw','campaign_convtype_performance_report') }}
     WHERE conversion_action_name = 'Google Shopping App Purchase (1)'
     GROUP BY 1,2,3,4,5
     UNION ALL
     SELECT DATE_TRUNC('year',date) as date, 'year' as date_granularity, 
     customer_id as account_id, name as campaign_name, id as campaign_id,
-    COALESCE(SUM(conversions),0) as purchases, COALESCE(SUM(conversions_value),0) as revenue
+    COALESCE(SUM(conversions),0) as purchases_adj, COALESCE(SUM(conversions_value),0) as revenue_adj
     FROM {{ source('googleads_raw','campaign_convtype_performance_report') }}
     WHERE conversion_action_name = 'Google Shopping App Purchase (1)'
     GROUP BY 1,2,3,4,5
-    ) raw
+    )
 USING (date,date_granularity,account_id,campaign_name,campaign_id)
